97e3db0157284045348211f6ca17c27e5c9ca7b8
Frederikam <frogkr@gmail.com>
2016-08-02 19:37:07 +0200

    Towers will now kill enemies before healing friendlies

--- dist/main.js
+++ dist/main.js
@@ -39,19 +39,18 @@ module.exports.loop = function () {
         for(var i in towers){
             var tower = towers[i];
             if(tower) {
-                var damagedCreeps = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
-                    filter:function(creep) {
-                        return creep.hits < creep.maxHits
-                    }
-                });
-                if(damagedCreeps){
-                    tower.heal(damagedCreeps)
+                var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if(closestHostile) {
+                    tower.attack(closestHostile);
                 } else {
-                    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-                    if(closestHostile) {
-                        tower.attack(closestHostile);
+                    var damagedCreeps = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+                        filter:function(creep) {
+                            return creep.hits < creep.hitsMax
+                        }
+                    });
+                    if(damagedCreeps){
+                        tower.heal(damagedCreeps)
                     } else {
-                    
                         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                             filter: (structure) => structure.hits < Math.min(10000, structure.hitsMax) && (structure.my || structure.structureType == STRUCTURE_ROAD)
                         });
