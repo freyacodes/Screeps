0a99b369b5e7d5f3d73760727c2fb137edf6ee89
Frederikam <frogkr@gmail.com>
2016-08-03 21:11:37 +0200

    Don't push to PTR

--- gruntfile.js
+++ gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
                 email: credentials.email,
                 password: credentials.password,
                 branch: 'v1.2',
-                ptr: true
+                ptr: false
             },
             dist: {
                 src: ['dist/*.js']
