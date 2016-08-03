41d69b9ebf39d846d445b348c991f9e78337ee38
Frederikam <frogkr@gmail.com>
2016-08-03 21:09:56 +0200

    Gruntfile now flattens folders before pushing to screeps, allowing a folder structure

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-screeps');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
     var credentials = grunt.file.readJSON('credentials.json');
 
     grunt.initConfig({
@@ -10,17 +13,40 @@ module.exports = function(grunt) {
                 email: credentials.email,
                 password: credentials.password,
                 branch: 'v1.2',
-                ptr: false
+                ptr: true
             },
             dist: {
                 src: ['dist/*.js']
             }
         },
+
         watch: {
           scripts: {
-          	files: ['dist/*.js'],
-            tasks: ['screeps']
+          	files: ['src/**/*.js'],
+            tasks: ['clean','copy','screeps']
           },
+        },
+
+        copy: {
+          screeps: {
+            files: [{
+              expand: true,
+              cwd: 'src/',
+              src: '**',
+              dest: 'dist/',
+              filter: 'isFile',
+              rename: function (dest, src) {
+                // Change the path name utilize dots for folders
+                return dest + src.replace(/\//g,'.');
+              }
+            }]
+          },
+        },
+
+        clean: {
+          'dist': ['dist']
         }
     });
+
+    grunt.registerTask('default', ['clean','copy','screeps']);
 }
