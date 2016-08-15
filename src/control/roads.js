f750d9f382bf30b823b50be42515d48ce7958829
Frederikam <frogkr@gmail.com>
2016-08-15 20:57:17 +0200

    Fixed cost matrix

--- src/control/roads.js
+++ src/control/roads.js
@@ -1,21 +1,79 @@
 module.exports = {
+
+    run: function(room){
+        var startCPU = Game.cpu.getUsed();
+
+        if(!room || !room.controller || !room.controller.my || room.controller.level >= 3){
+            return;
+        }
+
+        if(Game.cpu.tickLimit < 400){
+            return;
+        }
+
+        var startPoint = room.find(FIND_MY_SPAWNS)[1].pos;
+        var endPoints = [
+            room.controller.pos
+        ];
+
+        var sources = room.find(FIND_SOURCES);
+        for(var i in sources){
+            var source = sources[i];
+            endPoints[endPoints.length] = source.pos;
+        }
+
+        var endPoint = endPoints[Math.floor((Math.random() * endPoints.length))];
+
+        var path = PathFinder.search(startPoint, endPoint, {
+            roomCallback: this.createRoadsCostMatrix,
+            plainCost: 2,
+            swampCost: 9,
+            maxRooms: 3
+        });
+
+        var positions = path.path;
+        for(var i in positions){
+            var pos = positions[i];
+
+            var isPositionInvalid = false;
+            var tileContents = pos.look();
+            for(var i in tileContents){
+                if(tileContents.type == "structure" || tileContents.type == "constructionSite"){
+                    isPositionInvalid = true;
+                    continue;
+                }
+            }
+
+            if(!isPositionInvalid){
+                pos.placeFlag("Site_"+STRUCTURE_ROAD+"_"+pos);
+            }
+        }
+
+        var cpuUsage = Game.cpu.getUsed() - startCPU;
+    },
+
     createRoadsCostMatrix: function(roomName){
         var room = Game.rooms[roomName];
-        var spawn = room.find(FIND_MY_SPAWNS);
+        var spawn = room.find(FIND_MY_SPAWNS)[0];
 
-        var spawnX = spawn.x;
-        var spawnY = spawn.y;
+        if(spawn == null){
+            error("No spawn found");
+        }
+
+        var spawnX = spawn.pos.x;
+        var spawnY = spawn.pos.y;
 
         var costs = new PathFinder.CostMatrix;
         var data = room.lookAtArea(0, 0, 49, 49);
 
         for (x = 0; x < 50; x++) {
             for (y = 0; y < 50; y++) {
-                var tData = data[x][y];
+                var tData = data[y][x];
 
-                var structure = getTypeFromData(tData, "structure");
+                var structure = this.getTypeFromData(tData, "structure");
 
                 if(structure && structure.structureType == STRUCTURE_ROAD){
+                    console.log("FOUND ROAD AT "+x+","+y+" but really at "+structure.pos);
                     costs.set(x, y, 1);
                     continue;
                 }
@@ -25,7 +83,7 @@ module.exports = {
                     continue;
                 }
 
-                var site = getTypeFromData(tData, "constructionSite");
+                var site = this.getTypeFromData(tData, "constructionSite");
 
                 if(site){
                     if(site.structureType == STRUCTURE_ROAD){
@@ -37,7 +95,7 @@ module.exports = {
                 }
 
                 //Try to enforce a checkered system in relation to the first spawn
-                if(Math.abs((spawnX + x) % 2 + (spawnY + y) % 2) != 1){
+                if(Math.abs((spawnX - x) % 2 + (spawnY - y) % 2) != 1){
                     //This goes against the guideline
                     costs.set(x, y, 7);
                 }//Else just 0
@@ -65,11 +123,11 @@ module.exports = {
                         color = COLOR_GREEN;
                     }else if(cost === 7){
                         color = COLOR_YELLOW;
-                    }else(cost > 7){
+                    }else{
                         color = COLOR_RED;
                     }
                     var pos = room.getPositionAt(x,y);
-                    pos.createFlag("Debug_"+x+":"+y, color);
+                    pos.createFlag("Debug_"+cost+"_"+x+","+y, color);
                 }
             }
         }
